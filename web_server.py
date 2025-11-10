4fba318614d9492840e814752c76df550bce7112
KKhamwiset <kritsakorn224@gmail.com>
2025-11-10 16:12:58 +0700

    Read PORT env on render and add fallback port

--- web_server.py
+++ web_server.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from threading import Thread
-
+import os
 app = Flask('General เบ๊ Bot')
 @app.route('/')
 def home():
     return "General เบ๊ Bot is running!"
 
 def run(): 
-    app.run(host='0.0.0.0', port=8080) 
+    port = int(os.environ.get('PORT', 8080)) 
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
