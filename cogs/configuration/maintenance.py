5e7bdf2f35b420b3ddf8a8dd988e2d6237710162
KKhamwiset <kritsakorn224@gmail.com>
2025-11-25 00:05:38 +0700

    Revert status

--- cogs/configuration/maintenance.py
+++ cogs/configuration/maintenance.py
@@ -9,26 +9,37 @@ class Maintenance(commands.Cog):
         self.bot = bot
     @commands.hybrid_command(name="pause", help="Pauses the bot (Maintenance Mode).")
     @validation.role()
-    async def pause_bot(self, ctx):
+    async def pause_bot(self, ctx, instance: str = None):
+        if instance is not None and instance not in ["Server","Development"]:
+            await ctx.send("⚠️ Invalid instance.")
+            return
+        if not instance:
+            instance = self.bot.instance
         if self.bot.is_paused:
             await ctx.send("⚠️ Bot is already paused.")
             return
-        
-        await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name="Maintenance Mode"))
-        await ctx.send(f"⏸️ {self.bot.instance} bot paused. Entering Maintenance Mode.")
-        self.bot.is_paused = True
-        logging.info(f"Bot paused by {ctx.author}")
+        if (self.bot.instance == instance):
+            await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Game(name="Maintenance Mode"))
+            await ctx.send(f"⏸️ {instance} bot paused. Entering Maintenance Mode.")
+            self.bot.is_paused = True
+            logging.info(f"Bot paused by {ctx.author}")
 
     @commands.hybrid_command(name="resume", help="Resumes the bot from Maintenance Mode.")
     @validation.role()
-    async def resume_bot(self, ctx):
+    async def resume_bot(self, ctx, instance: str = None):
+        if instance is not None and instance not in ["Server","Development"]:
+            await ctx.send("⚠️ Invalid instance.")
+            return
+        if not instance:
+            instance = self.bot.instance
         if not self.bot.is_paused:
             await ctx.send("⚠️ Bot is not paused.")
             return
-        self.bot.is_paused = False
-        await self.bot.change_presence(status=discord.Status.online)
-        await ctx.send(f"▶️ {self.bot.instance} bot resumed. Back online!")
-        logging.info(f"Bot resumed by {ctx.author}")
+        if (self.bot.instance == instance):
+            self.bot.is_paused = False
+            await self.bot.change_presence(status=discord.Status.online,activity=discord.Game(None))    
+            await ctx.send(f"▶️ {instance} bot resumed. Back online!")
+            logging.info(f"Bot resumed by {ctx.author}")
 
 async def setup(bot):
     await bot.add_cog(Maintenance(bot))
