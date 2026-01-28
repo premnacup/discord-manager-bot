ff98ea3eaabc680e692b3eb942c622566da8e5ae
KKhamwiset <kritsakorn224@gmail.com>
2026-01-28 19:53:57 +0700

    Fix import error

--- cogs/academic/schedule.py
+++ cogs/academic/schedule.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from components.pagination_view import PaginationView, ExamEmbedStrategy, ClassEmbedStrategy
 from components.schedule_components import * 
 from validation import resolve_members 
-import re, datetime
+import re, datetime , os
 import requests
 from bs4 import BeautifulSoup
 # --------------------------------------------------
